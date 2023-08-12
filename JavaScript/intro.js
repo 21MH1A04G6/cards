f0ffa656e8e60e46b9ab90bc81dc94e566333ae3
21A91A04F4 <21a91a04f4@aec.edu.in>
2023-08-12 16:57:24 +0530

    first

---
+++ JavaScript/intro.js
@@ -0,0 +1,6 @@
+// var name = 'Navin';
+//         document.write(name.length);
+//         document.write("<br>");
+//         name += ' sim';
+//         document.write(name.length);
+//         //Navin//
